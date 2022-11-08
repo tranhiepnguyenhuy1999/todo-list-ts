1d7f791fae78c69e7d47087a75cf3471b8eeff4b
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2022-11-08 16:13:05 +0700

    add new features
    - clear all todos
    - remove successful todo in the list
    - add todo by pressing enter button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
+interface headerData {
+  children: string;
+}
 
-function App() {
+function Header({ children }: headerData) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <header className="header">
+      <h1>{children}</h1>
+    </header>
+  );
+}
+function ListToDo({
+  data,
+  setData
+}: {
+  data: string[];
+  setData: React.Dispatch<React.SetStateAction<string[]>>;
+}) {
+  function deleteOneTodos(id: number) {
+    setData(data.filter((i, index) => id !== index));
+  }
+  return (
+    <ul className="todo-list">
+      {data.map((i, index) => (
+        <li key={`toDo_item_${index}`}>
+          {i}
+          <span onClick={() => deleteOneTodos(index)}>x</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+function App() {
+  const [todoList, settodoList] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <div>
+      <Header>Todo List</Header>
+      <section className="main">
+        <div style={{ display: "flex", width: "100%" }}>
+          <input
+            ref={inputRef}
+            placeholder="Add new todo"
+            onKeyDown={e => {
+              if (
+                e.keyCode === 13 &&
+                inputRef.current &&
+                inputRef.current.value
+              ) {
+                settodoList([...todoList, inputRef.current.value]);
+                inputRef.current.value = "";
+              }
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              if (inputRef.current && inputRef.current.value) {
+                settodoList([...todoList, inputRef.current.value]);
+                inputRef.current.value = "";
+              }
+            }}
+          >
+            Add
+          </button>
+        </div>
+        <ListToDo data={todoList} setData={settodoList} />
+        <div
+          style={{ display: "flex", width: "100%", justifyContent: "flex-end" }}
         >
-          Learn React
-        </a>
-      </header>
+          <button onClick={() => settodoList([])}>Clear</button>
+        </div>
+      </section>
     </div>
   );
 }
